6f6a0f645c7d084bbf3f302b48dd06073f6f86da
Spencehiko <hikmet.kufteoglu@gmail.com>
2022-03-14 21:30:10 +0300

    minorfix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue';
-import { createPinia } from "pinia";
+import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import './assets/styles/index.css';
 const pinia = createPinia()
